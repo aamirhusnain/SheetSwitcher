b5e59d71c3879609021835b6191126c91ea06a15
amirhusnain.it@gmail.com <amirhusnain.it@gmail.com>
2023-04-24 16:26:30 +0500

    Updated Code

---
+++ SheetSwitcherWeb/Home.js
@@ -0,0 +1,62 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+
+
+
+    Office.onReady(function () {
+        var currentActiveSheet;
+        var listOfDeactiveSheets = [];
+
+        Excel.run(function (context) {
+            const sheet = context.workbook.worksheets;
+            sheet.onDeactivated.add(deactiveSheets);
+            return context.sync().then(function () {
+
+                //   console.log(currentActiveSheet);
+            });
+        });
+        deactiveSheets();
+
+        function deactiveSheets(event) {
+            listOfDeactiveSheets = [];
+            $scope.currentDeactiveSheets=[];
+         //   console.log(event.worksheetId);
+            //   console.log();
+            Excel.run(function (context) {
+                let activeSheet = context.workbook.worksheets.getActiveWorksheet();
+                let sheets = context.workbook.worksheets;
+                sheets.load("items/name");
+                return context.sync().then(function () {
+                    currentActiveSheet = activeSheet.load("name");
+                    
+                
+                    let totelSheets = sheets.items;
+                    console.log(totelSheets);
+                    for (let i = 0; i < totelSheets.length; i++) {
+                        if (totelSheets[i].id != currentActiveSheet.id) {
+                            listOfDeactiveSheets.push(totelSheets[i].name)
+                        } else {
+                            $scope.CurrentActiveSheetName = totelSheets[i].name;
+                        }
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    }
+                    console.log(listOfDeactiveSheets);
+                    $scope.currentDeactiveSheets = listOfDeactiveSheets;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                });
+            });
+        };
+
+
+    })
+
+
+
+
+
+});
+
